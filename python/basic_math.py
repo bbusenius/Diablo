f477ff50fe3f509febe4980ef89ff1c77c859812
Brad Busenius <bbusenius@users.noreply.github.com>
2014-06-17 23:25:02 -0500

    Added a function that calculates the slope of a line connecting two points on a grid

--- python/basic_math.py
+++ python/basic_math.py
@@ -368,4 +368,20 @@ def get_percentage(a, b, i=False, r=False):
         
     return percentage
 
- 
+def get_slope(x1, y1, x2, y2):
+    """
+    Calculate the slope of the line connecting two points on a grid.
+
+    Args:
+        x1: the x coordinate of the first point.
+
+        y1: the y coordinate of the first point
+
+        x:2 the x coordinate of the second point.
+        
+        y2: the y coordinate of the second point.
+
+    Returns:
+        the slope of a line connecting two points on a grid.
+    """
+    return (y2 - y1) / (x2 - x1) 
