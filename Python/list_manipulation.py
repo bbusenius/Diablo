55f5f83434dbe8fcc86ebb4ac7a5fee936b4bcf3
Brad Busenius <bbusenius@users.noreply.github.com>
2014-11-10 08:19:01 -0600

    Added an indentation variable to the python unserialize_php_array function.

--- Python/list_manipulation.py
+++ Python/list_manipulation.py
@@ -13,6 +13,11 @@ __status__ = "Testing"
 import phpserialize
 from collections import OrderedDict
 
+def recur(n, count=0):
+    if n == 0:
+        return "Finished count %s" % count
+    return recur(n-1, count+1)
+
 def unserialize_php_array(string):
     """Unserializes serialized php arrays and prints them to
     the console in an easy to read way. Only goes 1 level deep.
@@ -34,6 +39,8 @@ def unserialize_php_array(string):
         a php array.
         """
         retval = ''
+        indentation = ' ' * 3
+        
         # Base case - variable is not an iterable
         if hasattr(iterable,'__iter__') == False:
             non_iterable = str(iterable)
@@ -44,7 +51,7 @@ def unserialize_php_array(string):
         for item in iterable:
             # If item is a tuple it should be a key, value pair
             if str(type(item)) == "<type 'tuple'>" and len(item) == 2:
-
+                
                 # Get the key value pair
                 key = item[0]
                 val = loop_print(item[1])
@@ -55,12 +62,13 @@ def unserialize_php_array(string):
 
                 # There first item is an array key and the second item is an array
                 if hasattr(item[0],'__iter__') == False and hasattr(item[1],'__iter__') == True:
-                    retval += '\n   %s => array \n    ( \n%s    ),\n'  % (key, val)
+                    retval += '\n%s%s => array \n%s( \n%s%s),\n'  % (indentation, key, indentation, val, indentation)
                 else:
                     # Convert values to their properly formatted strings
                     # Integers and booleans are not quoted as array values
                     val = str(val) if val.isdigit() or val == 'True' or val == 'False' else '\'' + str(val) + '\''
-                    retval += '      %s => %s, \n'  % (key, val)
+                    retval += '%s%s => %s, \n'  % (indentation, key, val)
+
         return retval
 
     php_array = 'array (' + loop_print(data) + '\n);'
