8f0b6d15467911fdde04900233cc5ec75a0a7899
Brad Busenius <bbusenius@users.noreply.github.com>
2014-11-11 17:54:28 -0600

    Added multi-language support.

--- Python/list_manipulation.py
+++ Python/list_manipulation.py
@@ -5,27 +5,43 @@ This module has basic functions for the manipulation of lists and dictionaries.
 __author__  = "Brad Busenius"
 __copyright__ = "Copyright 2014"
 __credits__ = ['Brad Busineus']
-__license__ = "unlicensed"
 __version__ = "0.0.1"
 __maintainer__ = "Brad Busenius"
 __status__ = "Testing"
 
 import phpserialize
 from collections import OrderedDict
+import pprint
 
-def unserialize_php_array(string):
+def unserialize_php_array(string, language, level=3):
     """Unserializes serialized php arrays and prints them to
     the console in an easy to read way. Only goes 1 level deep.
     You should build multi-level support in the future.
 
     Args:
         string: a string of serialized php
+    
+        language: a string representing the desired output 
+        format for the array.
 
     Returns:
         None, prints a sequence of strings to the console."""
 
     # Serialized data converted to a python data structure (list of tuples)
     data = phpserialize.loads(string, array_hook=list)
+
+    # Language specific translations for some values
+    translate = {'php': { 'True'  : 'true', 
+                           'False' : 'false',
+                           'None'  : 'null'},
+                 'javascript' : {'True' : 'true',
+                                 'False' : 'false',
+                                 'None' : 'null'}}
+
+    # If language is python, use pprint library and avoid recursion entirely
+    if language == 'python':
+        pp = pprint.PrettyPrinter(indent=level)
+        return pp.pprint(data)
 
     def loop_print(iterable, level=3):
         """
@@ -39,7 +55,6 @@ def unserialize_php_array(string):
         # Base case - variable is not an iterable
         if hasattr(iterable,'__iter__') == False:
             non_iterable = str(iterable)
-            #print non_iterable
             return str(non_iterable)
          
         # Recursive case
@@ -50,24 +65,40 @@ def unserialize_php_array(string):
                 # Get the key value pair
                 key = item[0]
                 val = loop_print(item[1], level=level+3)
+        
+                # Translate special values
+                val = translate[language][val] if language in translate and val in translate[language] else val
  
                 # Convert keys to their properly formatted strings
                 # Integers are not quoted as array keys
                 key = str(key) if isinstance(key, (int, long)) else '\'' + str(key) + '\''
 
-                # There first item is a key and the second item is an iterable
-                if hasattr(item[0],'__iter__') == False and hasattr(item[1],'__iter__') == True:
-                    retval += '%s%s => array \n%s( \n%s%s),\n'  % (indentation, key, indentation, val, indentation)
-                # The second item is not an iterable
-                else:
-                    # Convert values to their properly formatted strings
-                    # Integers and booleans are not quoted as array values
-                    val = str(val) if val.isdigit() or val == 'True' or val == 'False' else '\'' + str(val) + '\''
-                    retval += '%s%s => %s, \n'  % (indentation, key, val)
+                # The first item is a key and the second item is an iterable, boolean
+                needs_unpacking = hasattr(item[0],'__iter__') == False and hasattr(item[1],'__iter__') == True 
+
+                # Get inner templates by language
+                if language == 'php':
+                    if needs_unpacking:
+                        retval += '%s%s => array \n%s( \n%s%s),\n'  % (indentation, key, indentation, val, indentation)
+                    # The second item is not an iterable
+                    else:
+                        # Convert values to their properly formatted strings
+                        # Integers and booleans are not quoted as array values
+                        val = str(val) if val.isdigit() or val in translate[language].values() else '\'' + str(val) + '\''
+                        retval += '%s%s => %s, \n'  % (indentation, key, val)
+                if language == 'javascript':
+                    if needs_unpacking:
+                        retval += '%s%s : {\n%s\n%s},\n' % (indentation, key, val, indentation)
+                    else:
+                        retval += '%s%s: %s,\n'  % (indentation, key, val)
 
         return retval
 
-    php_array = 'array (\n' + loop_print(data) + '\n);'
-    print php_array
-    return php_array
+    # Language specific outer template
+    template = {'php' : 'array (\n' + loop_print(data) + '\n);',
+                'javascript' : 'var jsObject = {\n' + loop_print(data) + '\n}'}
+
+    array = template[language]
+    print array
+    return array
 
