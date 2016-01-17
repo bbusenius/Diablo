80457fe8a3120dfe61fae254a1428652bf7b2994
Brad Busenius <bradb@neomailbox.ch>
2016-01-16 23:34:50 -0600

    Added a setup.py and MANIFEST.in and made the library installable.

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='diablo_python',
+    description='Simple libraries and repurposable code ' + \
+                'for inclusion in projects and general use.',
+    version='0.0.1',
+    author='Brad Busenius',
+    packages = find_packages(),
+    #py_modules=[
+    #    'convert_php', 
+    #    'file_parsing',
+    #    'simple_math', 
+    #], 
+    url='https://github.com/bbusenius/Diablo-Python',
+    license='BSD licence',
+    install_requires=[
+    ],
+    test_suite='tests',
+    zip_safe=False
+)
