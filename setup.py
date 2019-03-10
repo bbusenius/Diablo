4d8711c872b3dd67593ff3056ac3838e413da212
Brad Busenius <bradb@neomailbox.ch>
2019-03-09 18:07:57 -0600

    Added the ability to run get_percentage from the command line.

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name='diablo_python',
-    description='Simple libraries and repurposable code ' + \
-                'for inclusion in projects and general use.',
+    description='Simple libraries and repurposable code '
+    + 'for inclusion in projects and general use.',
     version='0.0.2',
     author='Brad Busenius',
     author_email='bbusenius@gmail.com',
-    packages = find_packages(),
-    entry_points = {
+    packages=find_packages(),
+    entry_points={
         'console_scripts': [
             'excel_to_html = commands:run_excel_to_html',
-        ],
+            'get_percentage = commands:run_get_percentage',
+        ]
     },
     url='https://github.com/bbusenius/Diablo-Python',
     license='GNU GPLv3, see LICENSE.txt',
-    install_requires=[
-        'beautifulsoup4',
-        'pandas',
-        'phpserialize',
-        'xlrd',
-    ],
+    install_requires=['beautifulsoup4', 'pandas', 'phpserialize', 'xlrd'],
     test_suite='tests',
-    zip_safe=False
+    zip_safe=False,
 )
